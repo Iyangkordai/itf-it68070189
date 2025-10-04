f0f0fdba20c98f931b1fe019e676267dfaf82b30
Iyangkordai <suphichaya.khanereo@gmail.com>
2025-10-04 21:17:22 +0700

    first draft

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 const num = document.getElementById("num")
+const current_log = document.getElementById("log")
+const money_wd = document.getElementById("money")
+const action = document.getElementById("bank_opt")
+let current_acc = document.getElementById("current_acc")
+let current_cash = document.getElementById("current_cash")
+let cnt = current_log.childElementCount
 
-function add() {
-    num.value = Number(num.value) + 1
+
+function change(){
+    const newP = document.createElement("p")
+    newP.textContent = (String(Number(cnt) + 1) + ", Current account balance:" + String(current_acc.value) + ", Current cash balance:" + String(current_cash.value))
+    current_log.appendChild(newP)
+    cnt = cnt + 1
+}
+
+function proceed(){
+    if (action == "Deposit"){
+        current_acc.value = Number(current_acc.value) + Number(money_wd.value)
+        current_cash.value = Number(current_cash.value) - Number(money_wd.value)
+    }
+    else{
+        current_acc.value = Number(current_acc.value) - Number(money_wd.value)
+        current_cash.value = Number(current_cash.value) + Number(money_wd.value)
+    }
+    change()
+    money_wd.value = 0
 }
